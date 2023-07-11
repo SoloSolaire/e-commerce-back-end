7b1582084c62d326911b928f923c03383ce3d247
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-07-10 22:27:41 -0700

    added get /:id to tags endpoint

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,8 +19,14 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{}],
+      include: [{ model: Product, through: ProductTag}],
     });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id.'});
+      return;
+    }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err)
   }
